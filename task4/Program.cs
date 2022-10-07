6d82d6b96ed645a9b713d2e4a3bff445744aaa7b
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-07 22:58:04 +0300

    кол.циф.в числе

---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿//колличество цифр в числе
+Console.Write("Введите число : ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+int i = 0;
+while (num > 0)
+{
+    i++;
+    num = num / 10;
+}
+Console.WriteLine("Количество цифр введенного числа : " + i);
