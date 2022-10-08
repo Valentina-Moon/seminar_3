06cc035e0076f490e91879c0796a5ab41fcd3f76
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-08 12:48:24 +0300

    таблица квадрата чисел

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿//таблица квадрата чисел от 1 до N
+Console.WriteLine("введите число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int a =1;
+while (a<=N)
+{
+    int b =a*a;
+    Console.WriteLine($"a = {a}, квадрат числа a = {b}");
+    a++;
+}
