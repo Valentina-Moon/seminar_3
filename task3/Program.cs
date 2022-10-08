42aef99396bde09199be56ff667fc8fdd49b33d5
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-08 13:07:39 +0300

    число  в натуральную степень  используя цикл

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Введите число A в натуральную степень B используя цикл.
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine() ?? "0");
+int count = 0;
+int C = 1;
+while (count<B)
+{
+    C = C * A;
+    count++;
+}
+Console.WriteLine($"Число {A} в степени {B} равно : {C}");
